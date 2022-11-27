aa98a0d1f4f0f7d8b7702990fefa17f00c809414
Christos Bandis <chr.bandis@gmail.com>
2022-11-27 18:09:41 +0200

    Update Graph.java

--- src/house_of_baratheon/Graph.java
+++ src/house_of_baratheon/Graph.java
@@ -1,6 +1,4 @@
-//Σε αυτό το σημείο του κώδικα καταφέραμε να δημιουργήσουμε ένα γράφο οποίος εμφανίζει ότι υπάρχει σχέση μεταξύ των
-//δύο nodes αλλά δε καταφέραμε να το κάνουμε να εμφανίζει τι είδους σχέση έχουν, οπότε καταλήξαμε στο να δημιουργήσουμε
-//και να χρησιμοποιήσουμε τη κλάση FindRelation.java
+//Not used 
 package house_of_baratheon;
 
 
@@ -13,41 +11,41 @@ public class Graph<Relation> {
 
     private Map<Relation,  List<Relation>> map = new HashMap<>();
 
-    public void addVertex(Relation s) { map.put(s, new LinkedList<Relation>());}   //Μέθοδος για τη δημιουργία vertex
+    public void addVertex(Relation s) { map.put(s, new LinkedList<Relation>());}   //Method for creating vertex
 
 
 
-    public void addEdge(Relation source, Relation destination, boolean bidirectional) { //Μέθοδος για τη δημιουργία edge
+    public void addEdge(Relation source, Relation destination, boolean bidirectional) { //Method to create edge
 
         if (!map.containsKey(source)) {
-            addVertex(source);                      //Εισαγωγή τιμής στο πρώτο vertex
+            addVertex(source);                      //Entering a value in the first vertex
 
         }
         if (!map.containsKey(destination)) {
-            addVertex(destination);                 //Εισαγωγή τιμής στο δέυτερο vertex
+            addVertex(destination);                 //Entering a value in the second vertex
 
         }
-        map.get(source).add(destination);           //Δημιουργία σύνδεσης - edge
+        map.get(source).add(destination);           //Create a connection - edge
 
         if (bidirectional == true) {
-            map.get(destination).add(source);      //Δημιουργία αντίστροφης σύνδεσης λόγω κατευθυνόμενου γράφου
+            map.get(destination).add(source);      //Creating a reverse connection due to a directed graph
         }
 
     }
 
-    public void hasEdge(Relation s, Relation d) {           //Εμφάνιση σχέσης μεταξύ των vertex
+    public void hasEdge(Relation s, Relation d) {           //Show relation between vertex
 
         if (map.get(s).contains(d)) {
-           System.out.println("Υπάρχει σχέση μεταξύ του/της " + s + " και του/της " + d + "." );
+           System.out.println("There is a relation between him/her " + s + " and him/her " + d + "." );
         } else {
-            System.out.println("Δεν υπάρχει σχέση μεταξύ του/της " + s + " και του/της " + d + ".");
+            System.out.println("There is no relation between him/her " + s + " and him/her " + d + ".");
         }
     }
 
-    public static void readcsv() { new ReadCSV().readCSV(); }       //Κλήση μεθόδων από άλλες κλάσεις
+    public static void readcsv() { new ReadCSV().readCSV(); }       //Call methods from other classes
     public void printGraph() {
 
-        readcsv();                           //Κλήση μεθόδου readcsv που είναι αναγκαία για τη ταξηνόμηση
+        readcsv();                           //Call readcsv method necessary for classification
         Graph g = new Graph();
 
 
@@ -74,15 +72,13 @@ public class Graph<Relation> {
         Scanner onoma2 = new Scanner(System.in);
 
 
-        System.out.print("Δώσε πρώτο όνομα: ");
+        System.out.print("Enter first name: ");
         name1 = onoma1.nextLine();
 
-        System.out.print("Δώσε δέυτερο όνομα: ");
+        System.out.print("Enter second name: ");
         name2 = onoma2.nextLine();
 
-         g.hasEdge(name1, name2);       //Κλήση μεθόδου για εμφάνιση σχέσης
+         g.hasEdge(name1, name2);       //Call the method to display a relation
 
     }
-
 }
-
