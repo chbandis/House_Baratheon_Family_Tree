250a4ff643a9e0d2d64ae071a388a9f95639f20d
Christos Bandis <chr.bandis@gmail.com>
2022-11-27 14:03:41 +0200

    Update House_Baratheon_Family_Tree.java

--- src/house_of_baratheon/House_Baratheon_Family_Tree.java
+++ src/house_of_baratheon/House_Baratheon_Family_Tree.java
@@ -1,4 +1,3 @@
-
 package house_of_baratheon;
 
 
@@ -6,8 +5,8 @@ import java.util.Scanner;
 
 public class House_Baratheon_Family_Tree {
 
-        public static void PrintCSV(){ new PrintCSV().printCSV(); }      //Κλήση μεθόδων από άλλες κλάσεις
-        //public static void PrintGraph() { new Graph().printGraph(); }  //Χρήση μεθόδου printGraph της κλάσης Graph.java που δε χρησιμοποιήθηκε
+        public static void PrintCSV(){ new PrintCSV().printCSV(); }      //Call methods from other classes
+        //public static void PrintGraph() { new Graph().printGraph(); }  //Call the printGraph method of the Graph.java class that was not used
         public static void SortCSV() { new SortCSV().SortByName(); }
         public static void WriteToFile() throws Exception { new WriteToFile().write();}
         public static void FindRelation() { new FindRelation().relfinder();}
@@ -17,17 +16,17 @@ public class House_Baratheon_Family_Tree {
 
             int choice;
             do {
-                System.out.println("Κεντρικό Μενού Εφαρμογής");
-                System.out.println("1. Εμφάνιση CSV αρχείου");
-                System.out.println("2. Ταξινόμηση των ανθρώπων κατά λεξικογραφική σειρά και εξαγωγή αρχείου txt");
-                System.out.println("3. Εύρεση Σχέσεων");
-                System.out.println("4. Εξαγωγή αρχείου .dot και οπτικοποίηση του σε αρχείο .jpg");
-                System.out.println("5. Έξοδος");
-                System.out.print("Επιλογή: ");
+                System.out.println("Main Application Menu");
+                System.out.println("1. View CSV file");
+                System.out.println("2. Sort people by lexicographical order and export txt file");
+                System.out.println("3. Find relation");
+                System.out.println("4. Export a .dot file and visualize it in a .jpg file");
+                System.out.println("5. Exit");
+                System.out.print("Select: ");
 
-                choice = input.nextInt();             //Εισαγωγή από το χρήστη
+                choice = input.nextInt();             //User input
 
-                                //Δημιουργία κεντρικού μενού και κλήση της κατάλληλης μεθόδου σύμφωνα με την επιλογή
+                                //Create a central menu and call the appropriate method according to the selection
                 switch (choice) {
                     case 1:
                         PrintCSV();
@@ -36,12 +35,12 @@ public class House_Baratheon_Family_Tree {
                     case 2:
                         SortCSV();
                         System.out.println("\n");
-                        System.out.println("Το αρχείο SortedCSV_Names&Genders εξήχθει επιτυχώς!");
+                        System.out.println("The SortedCSV_Names&Genders file was successfully exported!");
                         System.out.println("\n");
                         break;
                     case 3:
                         FindRelation();
-                        //PrintGraph();         //Μέθοδος printGraph που δε χρησιμοποιήθηκε
+                        //PrintGraph();         //Unused printGraph method
                         break;
                     case 4:
                         WriteToFile();
@@ -49,14 +48,13 @@ public class House_Baratheon_Family_Tree {
                         break;
                     case 5:
                         System.out.println("\n");
-                        System.out.println("Πραγματοποιείται έξοδος");
+                        System.out.println("Closing");
                         System.exit(0);
                         break;
                     default:
-                        System.out.println("Εισάγετε από 1 εώς 5");
+                        System.out.println("Enter from 1 to 5");
                         System.out.println("\n");
                 }
             } while(choice !=5);
         }
     }
-
